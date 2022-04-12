7a86dc423d410fc742abe972d79ae8c7ea82bafe
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2022-04-12 15:58:36 -0700

    added css for footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import Profile from './PortfolioContainer/Home/Profile';
 
+import Profile from './PortfolioContainer/Home/Profile';
+import Footer from './PortfolioContainer/Home/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Profile />
+      <Footer />
     </div>
   );
 }
---
+++ src/PortfolioContainer/Home/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Footer.css'
+
+
+
+export default function Footer() {
+  return (
+    <div className='footer-container'>
+        <div className='footer-parent'>
+            <img src={require ('../../../PortfolioContainer/assets/Home/shape-bg.png').default} 
+            alt="White"
+            />
+        </div>
+    </div>
+  )
+}
+   
